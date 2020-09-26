9ef076ce781f33574d3cb7d59ea81d000aeb1738
Amirreza1993 <50665160+Amirreza1993@users.noreply.github.com>
2020-09-26 10:37:12 +0200

    Update ScHiC-FS.py

--- ScHiC-FS.py
+++ ScHiC-FS.py
@@ -1,4 +1,3 @@
-
 import random
 from skfeature.function.similarity_based import reliefF
 from skfeature.function.similarity_based import lap_score
@@ -25,39 +24,38 @@ from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 ####################################IMPORT DATASET #######################3
 #CELL LINE Dataset 
-#DataMain=np.load("/home/nanni/Projects/SingleCellHiC/DI_indices/human_DIs.npy")
-DataMain=np.load("/home/amirreza/DI_indices22/human_DIs-cL_FULL.npy")
-data=DataMain
+data=np.load("") # location of .npy sample files
 cell_line={'HAP1':1, 'HeLa':2,'GM12878':3, 'K562':4}
-human_meta = pd.read_csv(os.path.join("./DI_indices", '/home/nanni/Projects/SingleCellHiC/DI_indices/human_meta.tsv'), sep="\t")
-human_meta.cell_line=[cell_line[item] for item in human_meta.cell_line]
-label=human_meta['cell_line']
+matrices_meta = pd.read_csv(os.path.join('matrices_meta/human_meta_cell-line.tsv'), sep="\t")
+matrices_meta.cell_line=[cell_line[item] for item in matrices_meta.cell_line]
+label=matrices_meta['cell_line']
 label=np.array(label)
 
-##CELL CYCLE Dataset
-#DataMain=np.load("/home/amirreza/Dataset/MAIN/Dataset/human_DIs-cc.npy")
-#data=DataMain
-#cell_cycle={'G1':1, 'ES':2,'MS':3, 'G2':4}
-#human_meta = pd.read_csv( '/home/amirreza/Dataset/MAIN/Dataset/human_meta_cellcycle.tsv', sep="\t")
-#human_meta.cell_cycle=[cell_cycle[item] for item in human_meta.cell_cycle]
-#label=human_meta['cell_cycle']
-#label=np.array(label)
+#CELL CYCLE Dataset
+DataMain=np.load("")# location of .npy sample files
+cell_cycle={'G1':1, 'ES':2,'MS':3, 'G2':4}
+matrices_meta = pd.read_csv( 'matrices_meta/human_meta_cellcycle.tsv', sep="\t")
+matrices_meta.cell_cycle=[cell_cycle[item] for item in matrices_meta.cell_cycle]
+label=matrices_meta['cell_cycle']
+label=np.array(label)
 
 
-#X_train, X_test, y_train, y_test = train_test_split(data, label, test_size=0.2, random_state=40)
-#y_train=y_train.astype(int)
-#y_test=y_test.astype(int)
+
+
+combination_method=1 #For AND operation :1 ,OR Operation :2, Majority Voting Operation :3 and Random selection 4
+threshold=1150 #Threshold for each feature selection method
+
 
 i=0
 acc=np.zeros(25) #to store the results of Accuracy
 ARI=np.zeros(25)#to store the results of ARI
-
+YP=[]
+YT=[]
+YS=[]
 # Use 5 fold cross validation
 kf = RepeatedKFold(n_splits=5, n_repeats=5) 
 X=data
 y=label
-
-
 for train_index, test_index in kf.split(X):
     
     X_train=X[train_index]
@@ -94,36 +92,40 @@ for train_index, test_index in kf.split(X):
 
   
     ###################################### Feature Integration
-    print('ens')
-    threshold=1150 #Threshold for each FS method
     idxM=idx_rel[:threshold]
     idxN=idx_lap[:threshold]
     idxO=idx_fish[:threshold]
-#AND
-    idx = set(idxO) - (set(idxO) - set(idxM)-set(idxN))
-    idx_and=reduce(np.intersect1d, (idxO, idxM, idxN))
-    print(idx_and.shape)
-#OR
-    idx=np.concatenate((idxM,idxN))
-    idx_or=np.unique(idx)
-    print(idx_or.shape)
-#MV
-    idx=np.concatenate((idxM,idxN,idxO))
-    c = Counter(idx)
-    a=c.most_common()
-    b=[i[0] for i in a if i[1]>1 ]
-    idx_mv=np.array(b)
-    print(idx_mv.shape)
 
-#Random selection
-    idx_rand=random.sample(range(0, np.shape(X_train)[1]), num_fea)
+    if combination_method==1 :
+    #AND
+        idx_and=reduce(np.intersect1d, (idxO, idxM, idxN))
+        idx=idx_and
+        print("number of selectes features (bins) = ", idx.shape[0])
+
+    if combination_method==2 :
+    #OR
+        idx=np.concatenate((idxM,idxN,idxO))
+        idx=np.unique(idx)
+        print("number of selectes features (bins) = ", idx.shape[0])
 
 
-    idx=idx_mv #define desired Integration method
-    num_fea = 250 #define number of feature to consider 
-    selected_features_train=X_train[:,idx[:num_fea]]
-    selected_features_test=X_test[:,idx[:num_fea]]
-    print('cl')
+    if combination_method==3 :
+    #MV
+        idx=np.concatenate((idxM,idxN,idxO))
+        c = Counter(idx)
+        a=c.most_common()
+        b=[i[0] for i in a if i[1]>1 ]
+        idx=np.array(b)
+        print("number of selectes features (bins) = ", idx.shape[0])
+
+    if combination_method==4 :
+    #Random selection
+        idx=random.sample(range(0, np.shape(X_train)[1]), threshold)
+        print("number of selectes features (bins) = ", idx.shape[0])
+
+    selected_features_train=X_train[:,idx]
+    selected_features_test=X_test[:,idx]
+    
     
     
 ###################################### Implementing the MLP classifier
@@ -135,9 +137,11 @@ for train_index, test_index in kf.split(X):
     solver='adam'
     mlp = MLPClassifier(hidden_layer_sizes=architecture,activation=activationf,learning_rate_init=learning_rate,solver=solver, max_iter=400)
     mlp.fit(selected_features_train,y_train) #training classifier
+    YT.append( y_test)
     y_predict = mlp.predict(selected_features_test)
+    YP.append( y_predict)
     acc[i] = accuracy_score(y_test, y_predict)
     y_score=mlp.predict_proba(selected_features_test)
+    YS.append(y_score)
     ARI[i]=adjusted_rand_score(y_test,y_predict)
     i=i+1
-  
